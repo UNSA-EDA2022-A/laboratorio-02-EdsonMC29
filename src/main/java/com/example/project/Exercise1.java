f93eeed26df07a127a35fe094ece9c003a82350b
Edson P. Miranda Callo <83046821+EdsonMC29@users.noreply.github.com>
2022-05-15 22:15:24 -0500

    version1

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -10,12 +10,29 @@ public class Exercise1 {
 		Scanner sc = new Scanner(System.in);
 		while (true) {
 			int n = sc.nextInt();
-			System.out.println(obj.esCuadradoPerfecto(n));			
+			System.out.println("Es cuadrado perfecto " + n + "? " + obj.esCuadradoPerfecto(n));			
 		}
 	}
 
 	public boolean esCuadradoPerfecto(int numero) {
 
-		return false;
+		int temp= numero;
+		boolean cuadrado = false;
+		int divisor = 2;
+			
+		//Forma iterativa y no recursiva
+	        while(temp != 1) {
+	            if(temp % divisor == 0){ //Factoriza el numero todos los 2
+	                System.out.print(divisor + " ");
+
+	                temp = temp / divisor;
+	                if(temp * temp == numero) // verifica si se encontro que es cuadrado perfecto 
+	                	cuadrado = true;
+	            }
+	            else {       
+	                divisor++;     // Factoriza ahora con los numeros siguientes
+	            }
+	        }
+		return cuadrado;
 	}
 }
